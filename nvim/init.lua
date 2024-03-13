85bd1f32f7336ed78e19c635adfcdc9ccbc40aec
Anton Sviridov <keynmol@gmail.com>
2024-03-13 10:59:56 +0000

    Update stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -36,7 +36,8 @@ local PLUGINS = {
       "sheerun/vim-polyglot",
       "tpope/vim-fugitive",
       "tpope/vim-commentary",
-      { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+      { "neandertech/nvim-langoustine", dir = "/Users/antonsviridov/projects/neandertech/nvim-langoustine" },
+      { "catppuccin/nvim",              name = "catppuccin",                                               priority = 1000 },
       "rebelot/kanagawa.nvim",
       {
         'nvim-lualine/lualine.nvim',
@@ -74,17 +75,17 @@ local PLUGINS = {
         'nvim-treesitter/nvim-treesitter-context',
         config = function()
           require 'treesitter-context'.setup {
-            enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-            max_lines = 5, -- How many lines the window should span. Values <= 0 mean no limit.
-            min_window_height = 50, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+            enable = true,           -- Enable this plugin (Can be enabled/disabled later via commands)
+            max_lines = 5,           -- How many lines the window should span. Values <= 0 mean no limit.
+            min_window_height = 50,  -- Minimum editor window height to enable context. Values <= 0 mean no limit.
             line_numbers = true,
             multiline_threshold = 1, -- Maximum number of lines to show for a single context
-            trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-            mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
+            trim_scope = 'outer',    -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+            mode = 'cursor',         -- Line used to calculate context. Choices: 'cursor', 'topline'
             -- Separator between context and content. Should be a single character string, like '-'.
             -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
             separator = nil,
-            zindex = 20, -- The Z-index of the context window
+            zindex = 20,     -- The Z-index of the context window
             on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
           }
         end
@@ -135,27 +136,26 @@ local PLUGINS = {
 
 local LUALINE = {
   setup = function()
-
     require('lualine').setup(
       {
-      sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff' },
-        lualine_c = { 'filename', {
-          'diagnostics',
-          sources = { 'nvim_diagnostic' },
-          symbols = { error = ' ', warn = ' ', info = ' ' },
-          diagnostics_color = {
-            color_error = { fg = '#ec5f67' },
-            color_warn = { fg = '#ECBE7B' },
-            color_info = { fg = '#008080' },
-          }
-        } },
-        lualine_x = { 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' }
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff' },
+          lualine_c = { 'filename', {
+            'diagnostics',
+            sources = { 'nvim_diagnostic' },
+            symbols = { error = ' ', warn = ' ', info = ' ' },
+            diagnostics_color = {
+              color_error = { fg = '#ec5f67' },
+              color_warn = { fg = '#ECBE7B' },
+              color_info = { fg = '#008080' },
+            }
+          } },
+          lualine_x = { 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' }
+        }
       }
-    }
     )
   end
 }
@@ -219,8 +219,8 @@ local CMP = {
       sorting = {
         priority_weight = 2,
         comparators = {
-          compare.offset, -- we still want offset to be higher to order after 3rd letter
-          compare.score, -- same as above
+          compare.offset,    -- we still want offset to be higher to order after 3rd letter
+          compare.score,     -- same as above
           compare.sort_text, -- add higher precedence for sort_text, it must be above `kind`
           compare.recently_used,
           compare.kind,
@@ -317,97 +317,6 @@ local METALS = {
   end
 }
 
---local NVIM_TREE = {
---  on_attach = function(bufnr)
---    local api = require('nvim-tree.api')
-
---    local function opts(desc)
---      return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
---    end
-
---    -- Default mappings. Feel free to modify or remove as you wish.
---    --
---    -- BEGIN_DEFAULT_ON_ATTACH
---    vim.keymap.set('n', '<C-]>', api.tree.change_root_to_node, opts('CD'))
---    vim.keymap.set('n', '<C-e>', api.node.open.replace_tree_buffer, opts('Open: In Place'))
---    vim.keymap.set('n', '<C-k>', api.node.show_info_popup, opts('Info'))
---    vim.keymap.set('n', '<C-r>', api.fs.rename_sub, opts('Rename: Omit Filename'))
---    vim.keymap.set('n', '<C-t>', api.node.open.tab, opts('Open: New Tab'))
---    vim.keymap.set('n', '<C-v>', api.node.open.vertical, opts('Open: Vertical Split'))
---    vim.keymap.set('n', '<C-x>', api.node.open.horizontal, opts('Open: Horizontal Split'))
---    vim.keymap.set('n', '<BS>', api.node.navigate.parent_close, opts('Close Directory'))
---    vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
---    vim.keymap.set('n', '<Tab>', api.node.open.preview, opts('Open Preview'))
---    vim.keymap.set('n', '>', api.node.navigate.sibling.next, opts('Next Sibling'))
---    vim.keymap.set('n', '<', api.node.navigate.sibling.prev, opts('Previous Sibling'))
---    vim.keymap.set('n', '.', api.node.run.cmd, opts('Run Command'))
---    vim.keymap.set('n', '-', api.tree.change_root_to_parent, opts('Up'))
---    vim.keymap.set('n', 'a', api.fs.create, opts('Create'))
---    vim.keymap.set('n', 'bmv', api.marks.bulk.move, opts('Move Bookmarked'))
---    vim.keymap.set('n', 'B', api.tree.toggle_no_buffer_filter, opts('Toggle No Buffer'))
---    vim.keymap.set('n', 'c', api.fs.copy.node, opts('Copy'))
---    vim.keymap.set('n', 'C', api.tree.toggle_git_clean_filter, opts('Toggle Git Clean'))
---    vim.keymap.set('n', '[c', api.node.navigate.git.prev, opts('Prev Git'))
---    vim.keymap.set('n', ']c', api.node.navigate.git.next, opts('Next Git'))
---    vim.keymap.set('n', 'd', api.fs.remove, opts('Delete'))
---    vim.keymap.set('n', 'D', api.fs.trash, opts('Trash'))
---    vim.keymap.set('n', 'E', api.tree.expand_all, opts('Expand All'))
---    vim.keymap.set('n', 'e', api.fs.rename_basename, opts('Rename: Basename'))
---    vim.keymap.set('n', ']e', api.node.navigate.diagnostics.next, opts('Next Diagnostic'))
---    vim.keymap.set('n', '[e', api.node.navigate.diagnostics.prev, opts('Prev Diagnostic'))
---    vim.keymap.set('n', 'F', api.live_filter.clear, opts('Clean Filter'))
---    vim.keymap.set('n', 'f', api.live_filter.start, opts('Filter'))
---    vim.keymap.set('n', 'g?', api.tree.toggle_help, opts('Help'))
---    vim.keymap.set('n', 'gy', api.fs.copy.absolute_path, opts('Copy Absolute Path'))
---    vim.keymap.set('n', 'H', api.tree.toggle_hidden_filter, opts('Toggle Dotfiles'))
---    vim.keymap.set('n', 'I', api.tree.toggle_gitignore_filter, opts('Toggle Git Ignore'))
---    vim.keymap.set('n', 'J', api.node.navigate.sibling.last, opts('Last Sibling'))
---    vim.keymap.set('n', 'K', api.node.navigate.sibling.first, opts('First Sibling'))
---    vim.keymap.set('n', 'm', api.marks.toggle, opts('Toggle Bookmark'))
---    vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
---    vim.keymap.set('n', 'O', api.node.open.no_window_picker, opts('Open: No Window Picker'))
---    vim.keymap.set('n', 'p', api.fs.paste, opts('Paste'))
---    vim.keymap.set('n', 'P', api.node.navigate.parent, opts('Parent Directory'))
---    vim.keymap.set('n', 'q', api.tree.close, opts('Close'))
---    vim.keymap.set('n', 'r', api.fs.rename, opts('Rename'))
---    vim.keymap.set('n', 'R', api.tree.reload, opts('Refresh'))
---    vim.keymap.set('n', 's', api.node.run.system, opts('Run System'))
---    vim.keymap.set('n', 'S', api.tree.search_node, opts('Search'))
---    vim.keymap.set('n', 'U', api.tree.toggle_custom_filter, opts('Toggle Hidden'))
---    vim.keymap.set('n', 'W', api.tree.collapse_all, opts('Collapse'))
---    vim.keymap.set('n', 'x', api.fs.cut, opts('Cut'))
---    vim.keymap.set('n', 'y', api.fs.copy.filename, opts('Copy Name'))
---    vim.keymap.set('n', 'Y', api.fs.copy.relative_path, opts('Copy Relative Path'))
---    vim.keymap.set('n', '<2-LeftMouse>', api.node.open.edit, opts('Open'))
---    vim.keymap.set('n', '<2-RightMouse>', api.tree.change_root_to_node, opts('CD'))
---    -- END_DEFAULT_ON_ATTACH
-
-
---    -- Mappings migrated from view.mappings.list
---    --
---    -- You will need to insert "your code goes here" for any mappings with a custom action_cb
---    vim.keymap.set('n', 'U', api.tree.change_root_to_parent, opts('Up'))
---  end,
-
---  setup = function()
---    require("nvim-tree").setup({
---      on_attach = on_attach,
---      sort_by = "case_sensitive",
---      renderer = {
---        group_empty = true,
---      },
---      filters = {
---        dotfiles = false,
---      },
---    })
-
---    vim.cmd([[nnoremap <C-a> :NvimTreeFindFile<CR>]])
---    vim.cmd([[nnoremap <C-t> :NvimTreeToggle<CR>]])
---  end
---}
-
-
--- require("settings.galaxyline").setup()
 
 
 local local_overrides = FUNCTIONS.prequire("locals")
@@ -446,7 +355,7 @@ local TREE_SITTER = {
       -- ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "scala", "rust", "go", "cpp" },
       sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
       highlight = {
-        enable = true, -- false will disable the whole extension
+        enable = true,      -- false will disable the whole extension
         additional_vim_regex_highlighting = false,
       },
       indent = {
@@ -656,11 +565,11 @@ local TELESCOPE = {
     vim.keymap.set('n', 'gds', B.lsp_document_symbols)
     vim.keymap.set('n', 'gws',
       function()
-      B.lsp_dynamic_workspace_symbols({
-        path_display = { shorten = { len = 1, exclude = { 1, -1 } } },
-        layout_strategy = 'vertical'
-      })
-    end)
+        B.lsp_dynamic_workspace_symbols({
+          path_display = { shorten = { len = 1, exclude = { 1, -1 } } },
+          layout_strategy = 'vertical'
+        })
+      end)
     vim.keymap.set('n', '<leader>mc', EXT.metals.commands)
 
     local previewers = require("telescope.previewers")
@@ -703,7 +612,7 @@ local TELESCOPE = {
             end
             return displayer {
               { p[#p], "TelescopeResultsNumber" },
-              { rest, "TelescopeResultsComment" },
+              { rest,  "TelescopeResultsComment" },
             }
           else
             return p[1]
@@ -773,6 +682,9 @@ local LSP_KEY_BINDINGS = {
     vim.keymap.set("n", "[e", function() vim.diagnostic.goto_prev { severity = vim.diagnostic.severity.ERROR } end)
     vim.keymap.set("n", "]w", function() vim.diagnostic.goto_next { severity = vim.diagnostic.severity.WARNING } end)
     vim.keymap.set("n", "[w", function() vim.diagnostic.goto_prev { severity = vim.diagnostic.severity.WARNING } end)
+    vim.keymap.set("n", "<leader>ih", function()
+      vim.lsp.inlay_hint.enable(0, not vim.lsp.inlay_hint.is_enabled(0))
+    end)
     vim.keymap.set("n", "<leader>ld", function() vim.diagnostic.open_float(0, { scope = "line" }) end)
     vim.keymap.set("n", "<leader>awf", vim.lsp.buf.add_workspace_folder)
   end
@@ -858,36 +770,36 @@ local INDENT_BLANKLINE = {
   end
 }
 
-local HARPOON = {
-  setup = function()
-    local harpoon = require("harpoon")
+-- local HARPOON = {
+--   setup = function()
+--     local harpoon = require("harpoon")
 
-    -- REQUIRED
-    harpoon:setup()
-    -- REQUIRED
-    -- lua require("harpoon.mark").add_file()
-    vim.keymap.set("n", "<leader>hm", function() harpoon:list():append() end)
-    local conf = require("telescope.config").values
-    local function toggle_telescope(harpoon_files)
-      local file_paths = {}
-      for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-      end
+--     -- REQUIRED
+--     harpoon:setup()
+--     -- REQUIRED
+--     -- lua require("harpoon.mark").add_file()
+--     vim.keymap.set("n", "<leader>hm", function() harpoon:list():append() end)
+--     local conf = require("telescope.config").values
+--     local function toggle_telescope(harpoon_files)
+--       local file_paths = {}
+--       for _, item in ipairs(harpoon_files.items) do
+--         table.insert(file_paths, item.value)
+--       end
 
-      require("telescope.pickers").new({}, {
-        prompt_title = "Harpoon",
-        finder = require("telescope.finders").new_table({
-          results = file_paths,
-        }),
-        previewer = conf.file_previewer({}),
-        sorter = conf.generic_sorter({}),
-      }):find()
-    end
+--       require("telescope.pickers").new({}, {
+--         prompt_title = "Harpoon",
+--         finder = require("telescope.finders").new_table({
+--           results = file_paths,
+--         }),
+--         previewer = conf.file_previewer({}),
+--         sorter = conf.generic_sorter({}),
+--       }):find()
+--     end
 
-    vim.keymap.set("n", "<leader>ht", function() toggle_telescope(harpoon:list()) end,
-      { desc = "Open harpoon window" })
-  end
-}
+--     vim.keymap.set("n", "<leader>ht", function() toggle_telescope(harpoon:list()) end,
+--       { desc = "Open harpoon window" })
+--   end
+-- }
 
 local SOURCEGRAPH = {
   setup = function()
@@ -923,7 +835,7 @@ LSP_KEY_BINDINGS.setup()
 CMP.setup()
 LUALINE.setup()
 KEY_BINDINGS.setup()
-HARPOON.setup()
+-- HARPOON.setup()
 LSP_SAGA.setup()
 INDENT_BLANKLINE.setup()
 SOURCEGRAPH.setup()
